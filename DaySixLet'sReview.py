e98837714f2ee43975b36f2c93992ca692bdd056
Ajay Singh <42219497+maverick27031989@users.noreply.github.com>
2020-05-14 19:01:48 -0400

    Add files via upload

---
+++ DaySixLet'sReview.py
@@ -0,0 +1,18 @@
+
+N = int(input())
+
+for l in range(0, N):
+    s = input()
+    lst1 = []
+    lst2 = []
+
+    for i in range(0, len(s)):
+        if i == 0 or i % 2 == 0:
+            lst1.append(s[i])
+
+        else:
+            lst2.append(s[i])
+
+    print(''.join(lst1) + ' ' + ''.join(lst2))
+
+
